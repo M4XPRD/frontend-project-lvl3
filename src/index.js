9e197de4bc0bcbc4a1862a640bcbd5a803437c52
Max <ppmax@bk.ru>
2022-07-29 18:13:16 +0300

    Updated validation code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import validation from './validation';
 
--- src/validation.js
+++ src/validation.js
@@ -1,46 +1,62 @@
 import * as yup from 'yup';
+import onChange from 'on-change';
 
-const userSchema = yup.string().url().required();
+const schema = yup.string().url().required();
 
-const validation = (fields) => {
-  try {
-    userSchema.validateSync(fields);
-    return 'This is valid';
-  }
-  catch (error) {
-    return 'This is not valid';
-  }
-};
+const validate = (url) => schema.validateSync(url);
 
 const messages = {
-  valid: {
-    main: 'RSS успешно загружен',
-  },
-  invalid: {
-    invalidURL: 'Ссылка должна быть валидным URL',
-    duplicate: 'RSS уже существует',
+  feedback: {
+    valid: {
+      main: 'RSS успешно загружен',
+    },
+    invalid: {
+      nonvalidURL: 'Ссылка должна быть валидным URL',
+      noRSS: 'Ресурс не содержит валидный RSS',
+      duplicate: 'RSS уже существует',
+    },
   },
 };
 
 const elements = {
-  form: document.querySelector('form'),
+  form: document.querySelector('.rss-form'),
   feedback: document.querySelector('.feedback'),
   input: document.querySelector('#url-input'),
 };
 
+const rssCheck = (url) => (String(url).indexOf('rss') !== -1);
+
 export default async () => {
   const state = {
-    link: '',
+    feed: [],
     status: null,
   };
 
-  const form = document.querySelector('.rss-form');
-  form.addEventListener('submit', async (e) => {
+  elements.form.addEventListener('submit', async (e) => {
+    e.preventDefault();
     const data = new FormData(e.target);
     const url = data.get('url');
-    if (validation(url) !== 'This is valid') {
-      elements.input.classList.add('is-invalid');
-      elements.feedback.textContent = messages.invalid.invalidURL;
+    switch (true) {
+      case (validate(url) === 'Nonvalid'):
+        elements.input.classList.add('is-invalid');
+        elements.feedback.textContent = messages.feedback.invalid.nonvalidURL;
+        return;
+      case (validate(url) === 'Valid' && rssCheck(url) === false):
+        elements.feedback.textContent = messages.feedback.invalid.noRSS;
+        elements.input.classList.remove('is-invalid');
+        elements.feedback.classList.replace('text-success', 'text-danger');
+        return;
+      case (validate(url) === 'Valid' && rssCheck(url) === true && state.feed.includes(url)):
+        elements.feedback.textContent = messages.feedback.invalid.duplicate;
+        elements.input.classList.remove('is-invalid');
+        elements.feedback.classList.replace('text-danger', 'text-success');
+        return;
+      default:
+        state.feed.push(url);
+        elements.feedback.textContent = messages.feedback.valid.main;
+        elements.input.classList.remove('is-invalid');
+        elements.feedback.classList.replace('text-danger', 'text-success');
+        elements.form.reset();
     }
   });
 };
