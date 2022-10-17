0db57c7cbc68771abc668705ebafea3ac2afa8da
Max <ppmax@bk.ru>
2022-10-18 00:28:51 +0300

    nothing important here really

--- test.js
+++ test.js
@@ -1,21 +1,44 @@
-import * as yup from 'yup';
+/* eslint-disable no-template-curly-in-string */
+// import * as yup from 'yup';
 
-const array = ['i', 'https://ya.ru/'];
+// import { setLocale } from 'yup';
 
-const validateURL = (url) => {
-  const schema = yup.string().url().required();
-  try {
-    schema.notOneOf(array).validateSync(url, { abortEarly: false });
-    return true;
-  } catch (error) {
-    return 'Ссылка не содержит валидный URL';
-  }
-};
+// setLocale({
 
-const ex1 = 'i';
-const ex2 = 'https://ru.hexl';
-const ex3 = 'https://ya.ru/';
+//   string: {
+//     min: 'Wtf dude lol 2',
+//   },
+// });
 
-console.log(validateURL(ex1));
-console.log(validateURL(ex2));
-console.log(validateURL(ex3));
+// // now use Yup schemas AFTER you defined your custom dictionary
+// const schema = yup.object().shape({
+//   name: yup.string().required().min(20),
+//   age: yup.number().min(18),
+// });
+
+// try {
+//   schema.validateSync({ name: 'ww', age: 11 });
+// } catch (err) {
+//   console.log(err.name); // => 'ValidationError'
+//   console.log(err.errors); // => ['Deve ser maior que 18']
+// }
+
+// const array = ['i', 'https://ya.ru/'];
+
+// const validateURL = (url) => {
+//   const schema = yup.string().url().required();
+//   try {
+//     schema.notOneOf(array).validateSync(url, { abortEarly: false });
+//     return true;
+//   } catch (error) {
+//     return 'Ссылка не содержит валидный URL';
+//   }
+// };
+
+// const ex1 = 'i';
+// const ex2 = 'https://ru.hexl';
+// const ex3 = 'https://ya.ru/';
+
+// console.log(validateURL(ex1));
+// console.log(validateURL(ex2));
+// console.log(validateURL(ex3));
