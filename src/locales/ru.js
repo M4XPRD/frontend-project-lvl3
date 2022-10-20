0e3bebdecbe83774a81011aaeb0c3d3c9fa39230
Max <ppmax@bk.ru>
2022-10-21 00:40:40 +0300

    added english locale

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,14 +1,16 @@
 export default {
   ru: {
     translation: {
-      valid: {
-        success: 'RSS успешно загружен',
-      },
-      invalid: {
-        defaultMessage: 'Некорретный URL',
-        nonvalidURL: 'Ссылка должна быть валидным URL',
-        noRSS: 'Ресурс не содержит валидный RSS',
-        duplicate: 'RSS уже существует',
+      validation: {
+        valid: {
+          success: 'RSS успешно загружен',
+        },
+        invalid: {
+          defaultMessage: 'Некорретный URL',
+          nonvalidURL: 'Ссылка должна быть валидным URL',
+          noRSS: 'Ресурс не содержит валидный RSS',
+          duplicate: 'RSS уже существует',
+        },
       },
     },
   },
