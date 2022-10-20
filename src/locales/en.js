7f6c92c15ca431d5d5f78a80658a300a46fa82d1
Max <ppmax@bk.ru>
2022-10-21 02:40:56 +0300

    added additional text for both languages

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,24 +1,23 @@
 export default {
-  en: {
-    translation: {
-      validation: {
-        valid: {
-          success: 'RSS uploaded successfully',
-        },
-        invalid: {
-          defaultMessage: 'Invalid URL',
-          nonvalidURL: 'The link must be a valid URL',
-          noRSS: 'The resource does not contain valid RSS',
-          duplicate: 'RSS already exists',
-        },
+  translation: {
+    validation: {
+      valid: {
+        success: 'RSS uploaded successfully',
       },
-      interface: {
-        h1: 'RSS Aggregator',
-        h2: "Start reading RSS today! It's easy, it's pretty.",
-        placeholder: 'RSS link',
-        example: 'Example: https://ru.hexlet.io/lessons.rss',
-        button: 'Add',
+      invalid: {
+        defaultMessage: 'Invalid URL',
+        nonvalidURL: 'The link must be a valid URL',
+        noRSS: 'The resource does not contain valid RSS',
+        duplicate: 'RSS already exists',
       },
+    },
+    interface: {
+      title: 'RSS Aggregator',
+      subtitle: "Start reading RSS today! It's easy, it's pretty.",
+      placeholder: 'RSS link',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+      button: 'Add',
+      hexlet: 'created by ',
     },
   },
 };
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,24 +1,23 @@
 export default {
-  ru: {
-    translation: {
-      validation: {
-        valid: {
-          success: 'RSS успешно загружен',
-        },
-        invalid: {
-          defaultMessage: 'Некорретный URL',
-          nonvalidURL: 'Ссылка должна быть валидным URL',
-          noRSS: 'Ресурс не содержит валидный RSS',
-          duplicate: 'RSS уже существует',
-        },
+  translation: {
+    validation: {
+      valid: {
+        success: 'RSS успешно загружен',
       },
-      interface: {
-        h1: 'RSS-агрегатор',
-        h2: 'Начните читать RSS сегодня! Это легко, это красиво.',
-        placeholder: 'ссылка RSS',
-        example: 'Пример: https://ru.hexlet.io/lessons.rss',
-        button: 'Добавить',
+      invalid: {
+        defaultMessage: 'Некорретный URL',
+        nonvalidURL: 'Ссылка должна быть валидным URL',
+        noRSS: 'Ресурс не содержит валидный RSS',
+        duplicate: 'RSS уже существует',
       },
+    },
+    interface: {
+      title: 'RSS-агрегатор',
+      subtitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      placeholder: 'Ссылка RSS',
+      example: 'Пример: https://ru.hexlet.io/lessons.rss',
+      button: 'Добавить',
+      hexlet: 'создано в ',
     },
   },
 };
