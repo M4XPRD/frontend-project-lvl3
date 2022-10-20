c61cdf1a9e655d6df4b25a4d5a4cc4865ea0ecab
Max <ppmax@bk.ru>
2022-10-21 00:40:51 +0300

    small refactoring

--- src/view.js
+++ src/view.js
@@ -1,18 +1,24 @@
-/* eslint-disable no-param-reassign */
+const renderInvalidFrame = (elements) => {
+  elements.input.classList.add('is-invalid');
+  elements.feedback.classList.replace('text-success', 'text-danger');
+};
+
+const renderValidFrame = (elements) => {
+  elements.input.classList.remove('is-invalid');
+  elements.feedback.classList.replace('text-danger', 'text-success');
+};
+
 const renderFeed = (elements, state, i18n) => {
   if (state.rssFeed.includes(state.field.url)) {
-    elements.feedback.textContent = i18n.t('invalid.duplicate');
-    elements.input.classList.add('is-invalid');
-    elements.feedback.classList.replace('text-success', 'text-danger');
+    elements.feedback.textContent = i18n.t('validation.invalid.duplicate');
+    renderInvalidFrame(elements);
   } else if (!state.valid && !state.rssFeed.includes(state.field.url)) {
-    elements.feedback.textContent = i18n.t('invalid.nonvalidURL');
-    elements.input.classList.add('is-invalid');
-    elements.feedback.classList.replace('text-success', 'text-danger');
+    elements.feedback.textContent = i18n.t('validation.invalid.nonvalidURL');
+    renderInvalidFrame(elements);
   } else {
     state.rssFeed.push(state.field.url);
-    elements.feedback.textContent = i18n.t('valid.success');
-    elements.input.classList.remove('is-invalid');
-    elements.feedback.classList.replace('text-danger', 'text-success');
+    elements.feedback.textContent = i18n.t('validation.valid.success');
+    renderValidFrame(elements);
     elements.form.reset();
     elements.input.focus();
   }
