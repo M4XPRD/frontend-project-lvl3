6ae8dd27c75582af57492f525b7e5bf24b8c2941
Max <ppmax@bk.ru>
2022-10-24 16:42:24 +0300

    refactoring

--- src/view.js
+++ src/view.js
@@ -9,30 +9,57 @@ const renderFrame = (elements, state) => {
 };
 
 const renderFeed = (elements, state, i18n) => {
-  if (state.rssFeed.includes(state.field.url)) {
-    console.log(`duplicate: state.errors: ${state.errors}`);
-    console.log(`duplicate: state.valid: ${state.valid}`);
-    console.log(`duplicate: if feed has url: ${state.rssFeed.includes(state.field.url)}`);
-    elements.feedback.textContent = i18n.t('validation.invalid.duplicate');
-    renderFrame(elements, state);
-  } else if (!state.valid && !state.rssFeed.includes(state.field.url)) {
-    console.log(`invalid: state.errors: ${state.errors}`);
-    console.log(`invalid: state.valid: ${state.valid}`);
-    console.log(`invalid: if feed has url: ${state.rssFeed.includes(state.field.url)}`);
-    elements.feedback.textContent = i18n.t(`${state.errors}`);
-    renderFrame(elements, state);
-  } else {
-    console.log(`valid: state.errors: ${state.errors}`);
-    console.log(`valid: state.valid: ${state.valid}`);
-    console.log(`valid: if feed has url: ${state.rssFeed.includes(state.field.url)}`);
-    state.errors = '';
-    state.rssFeed.push(state.field.url);
-    elements.feedback.textContent = i18n.t('validation.valid.success');
-    renderFrame(elements, state);
-    elements.form.reset();
-    elements.input.focus();
+  switch (true) {
+    case (!state.valid && !state.rssFeed.includes(state.field.url)):
+      elements.feedback.textContent = i18n.t(`${state.errors}`);
+      renderFrame(elements, state);
+      console.log(`INCORRECT: ${state.valid}`);
+      console.log(`INCORRECT URL: ${state.field.url}`);
+      break;
+    case (!state.valid && state.rssFeed.includes(state.field.url)):
+      elements.feedback.textContent = i18n.t('validation.invalid.duplicate');
+      renderFrame(elements, state);
+      console.log(`DUPLICATE: ${state.valid}`);
+      console.log(`DUPLICATE URL: ${state.field.url}`);
+      break;
+    default:
+      state.errors = '';
+      state.rssFeed.push(state.field.url);
+      elements.feedback.textContent = i18n.t('validation.valid.success');
+      renderFrame(elements, state);
+      console.log(`SUCCESS: ${state.valid}`);
+      console.log(`SUCCESS URL: ${state.field.url}`);
+      elements.form.reset();
+      elements.input.focus();
+      break;
   }
 };
+
+// const renderFeed = (elements, state, i18n) => {
+//   if (!state.valid && state.rssFeed.includes(state.field.url)) {
+//     console.log(`duplicate: state.errors: ${state.errors}`);
+//     console.log(`duplicate: state.valid: ${state.valid}`);
+//     console.log(`duplicate: if feed has url: ${state.rssFeed.includes(state.field.url)}`);
+//     elements.feedback.textContent = i18n.t('validation.invalid.duplicate');
+//     renderFrame(elements, state);
+//   } else if (!state.valid && !state.rssFeed.includes(state.field.url)) {
+//     console.log(`invalid: state.errors: ${state.errors}`);
+//     console.log(`invalid: state.valid: ${state.valid}`);
+//     console.log(`invalid: if feed has url: ${state.rssFeed.includes(state.field.url)}`);
+//     elements.feedback.textContent = i18n.t(`${state.errors}`);
+//     renderFrame(elements, state);
+//   } else {
+//     console.log(`valid: state.errors: ${state.errors}`);
+//     console.log(`valid: state.valid: ${state.valid}`);
+//     console.log(`valid: if feed has url: ${state.rssFeed.includes(state.field.url)}`);
+//     state.errors = '';
+//     state.rssFeed.push(state.field.url);
+//     elements.feedback.textContent = i18n.t('validation.valid.success');
+//     renderFrame(elements, state);
+//     elements.form.reset();
+//     elements.input.focus();
+//   }
+// };
 
 // const renderFeed = (elements, state, i18n) => {
 //   if (state.rssFeed.includes(state.field.url)) {
