8f61435d7297ad3aa88e2025d7a63408f2c145a7
Max <ppmax@bk.ru>
2022-10-26 19:58:23 +0300

    test files added

--- test.js
+++ test.js
@@ -1,96 +1,31 @@
 /* eslint-disable no-return-assign */
 /* eslint-disable no-unused-vars */
-import * as yup from 'yup';
-import _ from 'lodash';
-import onChange from 'on-change';
+// import * as yup from 'yup';
+// import onChange from 'on-change';
+import axios from 'axios';
 
-// const render = (state) => state.test += 1;
+const parseData = (url) => axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
+  .then((responce) => responce).catch(() => 'Ресурс не содержит валидный RSS');
 
-// const state = {
-//   valid: true,
-//   test: 0,
+parseData('http://lorem-rss.herokuapp.com/feed').then((responce) => console.log(responce.data));
+
+// const text = `
+// <html>
+//   <head>
+//     <title>New Document!</title>
+//   </head>
+//   <body>
+//     <h1 id="pageTitle">Random Text</h1>
+//     <p>What's up?</p>
+//   </body>
+// </html>
+// `;
+
+// const parse = () => {
+//   const parser = new DOMParser();
+//   const doc = parser.parseFromString(text, 'text/html');
+//   const title = doc.getElementById('pageTitle').textContent;
+//   console.log(title);
 // };
 
-// const watchedState = onChange(state, (path) => {
-//   if (path === 'valid') {
-//     render(state);
-//   }
-// });
-
-// watchedState.valid = false;
-// watchedState.valid = true;
-// console.log(state);
-
-yup.setLocale({
-  mixed: {
-    default: 'default',
-  },
-  string: {
-    url: 'invalid link',
-  },
-});
-
-const errors = [];
-
-const validateURL = (url) => {
-  const schema = yup.string().url().required();
-  try {
-    schema.notOneOf(['https://ya.ru/']).validateSync(url, { abortEarly: false });
-    console.log('success');
-    return true;
-  } catch (error) {
-    console.log(error.errors);
-    return false;
-  }
-};
-
-validateURL('htt');
-
-validateURL('https://ya.ru/');
-
-validateURL('https://ya.rssssu/');
-
-// console.log(errors);
-
-// setLocale({
-
-//   string: {
-//     min: 'Wtf dude lol 2',
-//   },
-// });
-
-// // now use Yup schemas AFTER you defined your custom dictionary
-// const schema = yup.object().shape({
-//   name: yup.string().required().min(20),
-//   age: yup.number().min(18),
-// });
-
-// const test = [];
-
-// try {
-//   schema.validateSync({ name: 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', age: 19 });
-// } catch (err) {
-//   console.log(err.name); // => 'ValidationError'
-//   test.push(...err.errors); // => ['Deve ser maior que 18']
-// }
-
-// console.log(_.isEmpty(test));
-// const array = ['i', 'https://ya.ru/'];
-
-// const validateURL = (url) => {
-//   const schema = yup.string().url().required();
-//   try {
-//     schema.notOneOf(array).validateSync(url, { abortEarly: false });
-//     return true;
-//   } catch (error) {
-//     return 'Ссылка не содержит валидный URL';
-//   }
-// };
-
-// const ex1 = 'i';
-// const ex2 = 'https://ru.hexl';
-// const ex3 = 'https://ya.ru/';
-
-// console.log(validateURL(ex1));
-// console.log(validateURL(ex2));
-// console.log(validateURL(ex3));
+// parse();
