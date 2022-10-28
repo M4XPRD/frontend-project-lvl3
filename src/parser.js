e79ea1d521c93e0e39732800e3778739cbb72ecc
Max <ppmax@bk.ru>
2022-10-28 03:21:54 +0300

    created parser file

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,28 @@
 import axios from 'axios';
 
 const parseURL = async (url) => {
-  axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
-    .then((responce) => responce.data)
+  axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+    .then((responce) => responce.data.contents) // загружено содержимое страницы
     .catch((err) => {
       throw new Error(err);
     });
 };
 
-const render = () => {};
+const parseRSS = (url) => {
+  parseURL(url).then((data) => {
+    const parser = new DOMParser();
+    console.log(data); // undefined ???
+    const doc = parser.parseFromString(url, 'application/xml');
+    console.log(doc); // parseError
+    // const titles = doc.querySelectorAll('title')[0]; // undefined
+    // console.log(titles);
+  });
+  // const parser = new DOMParser();
+  // const doc = parser.parseFromString(url, 'application/xml');
+  // const doc = parser.parseFromString(parseURL(url), 'application/xml');
+  // const doc = parser.parseFromString(url, 'application/xml');
+  // const doc = parser.parseFromString(url, 'text/html');
+  // const doc = parseURL(url).then((data) => parser.parseFromString(data, 'application/xml'));
+};
 
-export { parseURL, render };
+export { parseURL, parseRSS };
