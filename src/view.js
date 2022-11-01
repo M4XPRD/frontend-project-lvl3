879069fa94c73e7f30593dc24399dd43d3c2eeab
Max <ppmax@bk.ru>
2022-11-01 04:00:31 +0300

    big refactoring + added render for feed and posts

--- src/view.js
+++ src/view.js
@@ -1,22 +1,34 @@
 const renderFrame = (elements, state) => {
-  if (!state.valid) {
-    elements.input.classList.add('is-invalid');
-    elements.feedback.classList.replace('text-success', 'text-danger');
-  } else {
-    elements.input.classList.remove('is-invalid');
-    elements.feedback.classList.replace('text-danger', 'text-success');
+  switch (true) {
+    case !state.valid:
+      elements.input.classList.add('is-invalid');
+      elements.feedback.classList.replace('text-success', 'text-danger');
+      break;
+    case state.processState === 'parser error':
+      elements.input.classList.remove('is-invalid');
+      elements.feedback.classList.replace('text-success', 'text-danger');
+      break;
+    default:
+      elements.input.classList.remove('is-invalid');
+      elements.feedback.classList.replace('text-danger', 'text-success');
+      break;
   }
 };
 
 const renderInput = (elements, state, i18n) => {
   switch (true) {
-    case (!state.valid && !state.rssFeed.includes(state.field.url)):
+    case (state.valid && state.processState === 'parser error'):
+      elements.feedback.textContent = i18n.t('validation.invalid.noRSS');
+      elements.feedback.setAttribute('data-link-message', 'validation.invalid.noRSS');
+      renderFrame(elements, state);
+      break;
+    case (!state.valid && !state.rssFeedLinks.includes(state.field.url)):
       elements.feedback.textContent = i18n.t(`${state.errors}`);
       elements.feedback.setAttribute('data-link-message', `${state.errors}`);
       renderFrame(elements, state);
       state.processState = 'invalid link error';
       break;
-    case (!state.valid && state.rssFeed.includes(state.field.url)):
+    case (!state.valid && state.rssFeedLinks.includes(state.field.url)):
       elements.feedback.textContent = i18n.t('validation.invalid.duplicate');
       elements.feedback.setAttribute('data-link-message', 'validation.invalid.duplicate');
       renderFrame(elements, state);
@@ -24,16 +36,102 @@ const renderInput = (elements, state, i18n) => {
       break;
     default:
       state.errors = '';
-      state.rssFeed.push(state.field.url);
+      state.rssFeedLinks.push(state.field.url);
       elements.feedback.textContent = i18n.t('validation.valid.success');
       elements.feedback.setAttribute('data-link-message', 'validation.valid.success');
       renderFrame(elements, state);
-      state.processState = 'success';
       elements.form.reset();
       elements.input.focus();
+      state.processState = 'success';
       break;
   }
 };
+
+const renderFeed = (elements, state, i18n) => {
+  const feedsCard = document.createElement('div');
+  feedsCard.classList.add('card', 'border-0');
+  const feedsCardBody = document.querySelector('.feeds > .card > .card-body') ?? document.createElement('div');
+  feedsCardBody.classList.add('card-body');
+  const feedsCardTitle = document.querySelector('.feeds > .card > .card-body > .card-title') ?? document.createElement('h2');
+  feedsCardTitle.classList.add('card-title', 'h4');
+  feedsCardTitle.textContent = i18n.t('interface.feeds');
+
+  feedsCardBody.append(feedsCardTitle);
+  feedsCard.append(feedsCardBody);
+
+  const feedsListGroup = document.createElement('ul');
+  feedsListGroup.classList.add('list-group', 'border-0', 'rounded-0');
+  const feedsListGroupItem = document.createElement('li');
+  feedsListGroupItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+  const feedsListGroupItemTitle = document.createElement('h3');
+  feedsListGroupItemTitle.classList.add('h6', 'm-0');
+  feedsListGroupItemTitle.textContent = state.feeds.shift().textContent;
+  const feedsListGroupItemDescription = document.createElement('p');
+  feedsListGroupItemDescription.classList.add('m-0', 'small', 'text-black-50');
+  feedsListGroupItemDescription.textContent = state.feeds.shift().textContent;
+
+  feedsListGroup.append(feedsListGroupItem);
+  feedsCard.append(feedsListGroup);
+  feedsListGroupItem.append(feedsListGroupItemTitle);
+  feedsListGroupItem.append(feedsListGroupItemDescription);
+
+  feedsCard.append(feedsListGroup);
+
+  elements.feeds.prepend(feedsCard);
+};
+
+const renderPosts = (elements, state, i18n) => {
+  const postsCard = document.createElement('div');
+  postsCard.classList.add('card', 'border-0');
+  const postsCardBody = document.querySelector('.posts > .card > .card-body') ?? document.createElement('div');
+  postsCardBody.classList.add('card-body');
+  const postsCardTitle = document.querySelector('.posts > .card > .card-body > .card-title') ?? document.createElement('h2');
+  postsCardTitle.classList.add('card-title', 'h4');
+  postsCardTitle.textContent = i18n.t('interface.posts');
+
+  postsCardBody.append(postsCardTitle);
+  postsCard.append(postsCardBody);
+
+  const postsListGroup = document.createElement('ul');
+  postsListGroup.classList.add('list-group', 'border-0', 'rounded-0');
+
+  state.posts.forEach((item) => {
+    const itemTitle = item.querySelector('title').textContent;
+    const itemLink = item.querySelector('link').textContent;
+
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    const a = document.createElement('a');
+    console.log(item);
+    a.setAttribute('href', itemLink);
+    a.classList.add('fw-bold');
+    a.setAttribute('data-id', state.idCounter);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.textContent = itemTitle;
+
+    const modalButton = document.createElement('button');
+    modalButton.setAttribute('type', 'button');
+    modalButton.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    modalButton.setAttribute('data-id', state.idCounter);
+    modalButton.setAttribute('data-bs-toggle', 'modal');
+    modalButton.setAttribute('data-bs-target', '#modal');
+    modalButton.textContent = i18n.t('interface.view');
+
+    li.append(a);
+    li.append(modalButton);
+    postsListGroup.append(li);
+
+    state.idCounter += 1;
+  });
+
+  state.posts = Object.assign([]);
+
+  postsCard.append(postsListGroup);
+  elements.posts.prepend(postsCard);
+};
+
+const renderModal = () => {};
 
 const renderLanguage = (elements, value, previousValue, i18n) => {
   const previousLangButton = document.querySelector(`[data-lng="${previousValue}"]`);
@@ -50,6 +148,17 @@ const renderLanguage = (elements, value, previousValue, i18n) => {
   elements.interface.example.textContent = i18n.t('interface.example');
   elements.interface.hexlet.textContent = i18n.t('interface.hexlet');
   elements.feedback.textContent = i18n.t(feedbackMessageDataset);
+
+  const feeds = document.querySelector('.feeds > .card > .card-body > .card-title');
+  const posts = document.querySelector('.posts > .card > .card-body > .card-title');
+  feeds.textContent = i18n.t('interface.feeds');
+  posts.textContent = i18n.t('interface.posts');
+  const modalButtons = document.querySelectorAll('[data-bs-toggle="modal"]');
+  modalButtons.forEach((button) => {
+    button.textContent = i18n.t('interface.view');
+  });
 };
 
-export { renderInput, renderLanguage };
+export {
+  renderInput, renderLanguage, renderFeed, renderPosts, renderModal,
+};
