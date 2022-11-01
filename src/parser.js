80a142b40bdc8f4650132cc95abbfadc307864f7
Max <ppmax@bk.ru>
2022-11-01 04:00:06 +0300

    parser has been created

--- src/parser.js
+++ src/parser.js
@@ -1,28 +1,27 @@
 import axios from 'axios';
 
-const parseURL = async (url) => {
-  axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
-    .then((responce) => responce.data.contents) // загружено содержимое страницы
-    .catch((err) => {
-      throw new Error(err);
-    });
-};
+const parseURL = (url) => axios
+  .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+  .then((responce) => responce.data.contents)
+  .catch((err) => {
+    throw new Error(err);
+  });
 
-const parseRSS = (url) => {
+const parseRSS = (url, state) => {
   parseURL(url).then((data) => {
     const parser = new DOMParser();
-    console.log(data); // undefined ???
-    const doc = parser.parseFromString(url, 'application/xml');
-    console.log(doc); // parseError
-    // const titles = doc.querySelectorAll('title')[0]; // undefined
-    // console.log(titles);
+    const doc = parser.parseFromString(data, 'application/xml');
+    console.log(doc);
+    const feedsTitle = doc.querySelector('title');
+    const feedsDescription = doc.querySelector('description');
+    const isParseError = doc.querySelector('parsererror') ? 'parser error' : 'continue loading';
+    state.feeds.push(feedsTitle, feedsDescription);
+    const posts = doc.querySelectorAll('item');
+    posts.forEach((item) => {
+      state.posts.push(item);
+    });
+    state.processState = isParseError;
   });
-  // const parser = new DOMParser();
-  // const doc = parser.parseFromString(url, 'application/xml');
-  // const doc = parser.parseFromString(parseURL(url), 'application/xml');
-  // const doc = parser.parseFromString(url, 'application/xml');
-  // const doc = parser.parseFromString(url, 'text/html');
-  // const doc = parseURL(url).then((data) => parser.parseFromString(data, 'application/xml'));
 };
 
 export { parseURL, parseRSS };
