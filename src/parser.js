bbb864ab7336ca99e1fa7c0d4d28e9e67538687f
Max <ppmax@bk.ru>
2022-11-08 05:05:53 +0300

    parser refactoring

--- src/parser.js
+++ src/parser.js
@@ -10,25 +10,34 @@ const parseURL = (url) => axios
 const parseRSS = (data) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(data, 'application/xml');
-  // console.log(doc);
   const feedsTitle = doc.querySelector('title');
   const feedsDescription = doc.querySelector('description');
   const loadedFeeds = {
     feedsTitle, feedsDescription,
   };
   const posts = doc.querySelectorAll('item');
-  const arrayOfPosts = [...posts];
   const loadedPosts = [...posts].map((item) => {
     const postTitle = item.querySelector('title');
     const postDescription = item.querySelector('description');
     const postLink = item.querySelector('link');
     return { postTitle, postDescription, postLink };
   });
-  const isParseError = doc.querySelector('parsererror') ? 'parser error' : 'start to render';
+  const isParseError = doc.querySelector('parsererror') ? 'parser error' : 'continue render';
 
   return {
-    loadedFeeds, loadedPosts, isParseError, arrayOfPosts,
+    loadedFeeds, loadedPosts, isParseError,
   };
 };
 
-export { parseURL, parseRSS };
+const loadFeed = (link, currentState) => {
+  parseURL(link).then((responce) => {
+    const parserErrorCheck = parseRSS(responce).isParseError;
+    const feeds = parseRSS(responce).loadedFeeds;
+    const posts = parseRSS(responce).loadedPosts;
+    currentState.processState = parserErrorCheck;
+    currentState.parsedFeeds.unshift(feeds);
+    currentState.parsedPosts.unshift(posts);
+  });
+};
+
+export { parseURL, parseRSS, loadFeed };
